777100a177c74c7f56ca86211c790f1a35e29aa8
alex <you@example.com>
2020-02-27 20:03:30 +0300

    w3layout template added

--- weather/urls.py
+++ weather/urls.py
@@ -2,8 +2,11 @@
 from django.conf.urls import url
 from django.contrib import admin
 from app import views
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     path('',views.home, name='home')
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
